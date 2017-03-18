b3dfec89e865ddf9926ca04c31a4fe4782c902d6
Nicolae Sergiu Neamt <Nicolae Sergiu Neamt>
2017-03-18 11:53:30 +0200

    add simple function

--- pr01/main.cpp
+++ pr01/main.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+
+int add(int a, b) {
+	return a + b;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world!" << add(1,3) << endl;
     return 0;
 }
