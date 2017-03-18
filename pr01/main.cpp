0f83d168fdf60e1ffbe1b05ac254d1b86eec2ed3
Nicolae Sergiu Neamt <Nicolae Sergiu Neamt>
2017-03-18 11:58:13 +0200

    remove conflict

--- pr01/main.cpp
+++ pr01/main.cpp
@@ -9,7 +9,6 @@ int add(int a, b) {
 
 int main()
 {
-<<<<<<< HEAD
     int n;
 
     cout << "n = ";
@@ -19,8 +18,5 @@ int main()
         cout << i << " Hello world! " << endl;
     }
 
-=======
-    cout << "Hello world!" << add(1,3) << endl;
->>>>>>> b3dfec89e865ddf9926ca04c31a4fe4782c902d6
     return 0;
 }
