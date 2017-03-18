6b888eeb5fa49e12ca869cc2b7811380fadc6c7c
Nicolae Sergiu Neamt <Nicolae Sergiu Neamt>
2017-03-18 13:02:31 +0200

    Add more functions

--- pr01/main.cpp
+++ pr01/main.cpp
@@ -3,20 +3,68 @@
 using namespace std;
 
 
-int add(int a, b) {
-	return a + b;
+/**
+* Calculate the sum of digits from given number long
+*/
+int digitSum(long n) {
+    int s = 0;
+
+    while( n > 0) {
+        s += n % 10;
+        n /= 10;
+    }
+
+    return s;
 }
+
+/**
+* Calculate the reverse of given number long
+*/
+long reverse(long n) {
+    int s = 0;
+
+    while( n > 0) {
+        s = s * 10 + n % 10;
+        n /= 10;
+    }
+
+    return s;
+}
+
+/**
+* Calculate the number of digits
+*/
+int length(long n) {
+    int s = 0;
+
+    while( n > 0) {
+        s += 1;
+        n /= 10;
+    }
+
+    return s;
+}
+
 
 int main()
 {
-    int n;
+    long n,m;
 
     cout << "n = ";
     cin >> n;
 
-    for(int i=1; i<= n; i++ ) {
-        cout << i << " Hello world! " << endl;
+    m = reverse(n);
+    cout << "Inversul  este " << m << endl;
+    cout << "Length " << length(n) << endl;
+
+    if(n == m) {
+        cout << "Numarul este palindrom";
     }
+    else {
+        cout << "Numarul nu este palindrom";
+    }
+
+    cout << "\nDone!";
 
     return 0;
 }
